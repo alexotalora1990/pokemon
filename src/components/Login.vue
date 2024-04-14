a94cda006056d33b541496a0150f79c7da6d4d01
alexotalora1990 <alexotaloracorredor@gmail.com>
2024-04-13 20:51:05 -0500

    nombre pokemon login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="home">
-    <img class="imagenFondo" :src="imagenActual">  
-    <div class="Fondo">  
-       
+  <div class="home">     
+
       <div class="buttons-container">
         <button class="button" @click="irABuscarPokemon">Buscar Pokémon</button>
         <button class="button" @click="irAPlayPokemon">Adivinar Pokémon</button>
       </div>
-    </div>
+      <div class="containerImagen">
+        <p>{{ nombrePokemon }}</p>
+        <img class="imagenFondo1" :src="imagenActual">
+     
+      </div>
+
+    
+
   </div>
 </template>
 
@@ -18,6 +23,7 @@ import axios from 'axios';
 
 const router = useRouter();
 const imagenActual = ref('');
+const nombrePokemon = ref('');
 
 const irABuscarPokemon = () => {
   router.push('/BuscarPokemon');
@@ -27,8 +33,9 @@ const irAPlayPokemon = () => {
   router.push('/AdivinaPokemon');
 }
 
-onMounted(() => {
-  setInterval(cambiarImagen, 3000); // Cambia la imagen cada 3 segundos
+onMounted(async () => {
+  await cambiarImagen(); // Cargar la primera imagen al iniciar el componente
+  setInterval(cambiarImagen, 3000); // Cambiar la imagen cada 3 segundos
 });
 
 async function cambiarImagen() {
@@ -37,70 +44,77 @@ async function cambiarImagen() {
     const resultado = await axios.get(`https://pokeapi.co/api/v2/pokemon/${idAleatorio}`);
     const pokemonData = resultado.data;
     imagenActual.value = pokemonData.sprites.other['official-artwork'].front_default;
+    nombrePokemon.value = pokemonData.name;
   } catch (error) {
     console.log(error);
   }
 }
 </script>
 
+
 <style>
+
 body {
-  background: rgb(252,237,28);
-background: linear-gradient(-45deg, rgba(252,237,28,1) 0%, rgba(6,188,228,1) 100%);
+  background: rgb(252, 237, 28);
+  background: linear-gradient(-45deg, rgba(252, 237, 28, 1) 0%, rgba(6, 188, 228, 1) 100%);
 }
+
 .home {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
+  display: grid;
+  grid-template-columns: 40% 60%;
+    height: 100vh;
 }
 
 
-.imagenFondo{
-  margin-top: 5%;
-  margin-bottom: 5%;
+.containerImagen{
+  text-align: center;
+}
+.imagenFondo1 {
+   margin-bottom: 5%;
   transform: scale(1);
-  margin-left: 50%;
+  text-align: center;
+  
+}
+.containerImagen p{
+font-size: 400%;
+padding-top: 5%;
 }
 
-
-
-.buttons-container {
-  position: absolute;
-  top: 50%;
-  left: 30%;
-  transform: translate(-50%, -50%);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+.buttons-container {   
+    align-items: center;
+    margin-left: 20%;
+    padding-top: 20%;
 }
 
 .button {
-  font-size: 300%;
+  font-size: 280%;
   padding: 10px 20px;
-  margin: 10px;
+  margin: 6%;
   background-color: #fced1c;
   border: none;
-  border-radius: 5px;
+  border-radius: 10px;
   cursor: pointer;
   transition: background-color 0.3s ease;
 }
 
 .button:hover {
-  
- 
+
   background-color: #888229;
   color: #ffffff;
 
 }
 
-@media (max-width: 1000px){
+@media (max-width: 1000px) {
   .imagenFondo {
-  transform: scale(.7);  
+    transform: scale(.7);
   }
-  .button {
-  font-size: 200%;
-  }
-}
 
+  .button {
+    font-size: 200%;
+  }
+  .containerImagen p{
+font-size: 300%;
+
+}
+}
 </style>
