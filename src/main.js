22d91c59cf4ca69ff621ebe4fe08229aaa279beb
alexotalora1990 <146216150+alexotalora1990@users.noreply.github.com>
2024-04-13 21:15:14 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import {createPinia} from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 
 import { Quasar} from 'quasar'
